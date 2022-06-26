d2972bf48648966d2afad72f098829f07059e7a6
Mike <mikerking@protonmail.com>
2022-06-26 11:30:03 +0100

    Change home nav and site title to Overte Home and Overte Metaverse.

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@
                 </v-list-item-icon>
 
                 <v-list-item-content>
-                    <v-list-item-title>Vircadia Home</v-list-item-title>
+                    <v-list-item-title>Overte Home</v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
 
