90ac21ad29960c8dd979f13147ada8ebb1713fa0
loudened <loudened@users.noreply.github.com>
2023-07-24 11:58:03 +0100

    Update licence info

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 //
 //  Created by Kalila L. on 2 Jul 2020.
 //  Copyright 2020 Vircadia contributors.
-//
+//  Modified by Overte constributors (See git history for detailed changelog)
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 */
